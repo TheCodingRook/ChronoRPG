47473de6dffb4aac293db1703f73ceea36238cd7
CodingRook <CodingRook@gmail.com>
2020-04-22 15:15:10 +0100

    Minor object arranging in the World Outliner in the test map. Implemented a Grabbing ability C++ class (a physics handle component) to use when player grabs props.

---
+++ Source/ChronoRPG/GrabbingAbility.cpp
@@ -0,0 +1,93 @@
+// Copyright 2020. All rights reserved. A game prototype by CodingRook.
+
+
+#include "GrabbingAbility.h"
+#include "ChronoRPGCharacter.h"
+#include "InteractablePropBase.h"
+
+void UGrabbingAbility::GrabProp(AActor* In_AActor)
+{
+	OwnerCharacter = Cast<AChronoRPGCharacter>(GetOwner());
+
+	if (AInteractablePropBase* PropToGrab = Cast<AInteractablePropBase>(In_AActor))
+	{
+		// First check to see that we are not already holding on to something else
+		if (bIsAlreadyHoldingSomething)
+		{
+			// Are we holding the same thing we are trying to grab?
+			if (PropToGrab == PropAlreadyHeld)
+			{
+				return; // If so, do nothing and return
+			}
+			else
+			{
+				// We are holding something different, so drop that and will pick up new prop
+				DropProp();
+			}
+		}
+
+		// Save the pawn channel's collision resposnse setting before setting to "Ignore"
+		OriginalPawnECR = PropToGrab->GetMesh()->GetCollisionResponseToChannel(ECC_Pawn);
+		PropToGrab->GetMesh()->SetCollisionResponseToChannel(ECC_Pawn, ECR_Ignore);
+
+		// Pick up the prop, whether by phyics handle or attaching to a socket
+		if (PropToGrab->ActorHasTag(AttachableTag))
+		{
+			// This prop can be attached to a socket, but first stop simulating physics
+			PropToGrab->GetMesh()->SetSimulatePhysics(false);
+			PropToGrab->AttachToComponent(OwnerCharacter->GetMesh(), FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), "GrabSocket");
+		}
+		else
+		{
+			// We can alternatively pick this prop up using the default physics handle component
+			GrabComponentAtLocation(PropToGrab->GetMesh(), NAME_None, PropToGrab->GetMesh()->GetCenterOfMass());
+		}
+
+		// We are now grabbing/holding something, so set the necessary member fields
+		bIsAlreadyHoldingSomething = true;
+		PropAlreadyHeld = PropToGrab;
+	}
+}
+
+void UGrabbingAbility::DropProp()
+{
+	// First check if we are using the physics handle; if so, the pointer GrabbedComponent will be non-null
+	if (GrabbedComponent)
+	{
+		// Reset the collision channel for pawns back to original setting
+		GrabbedComponent->SetCollisionResponseToChannel(ECC_Pawn, OriginalPawnECR);
+		ReleaseComponent();
+	}
+	// Otherwise let's see if we have a prop that we are grabbing (the PropAlreadyHeld is non-null)
+	else if(PropAlreadyHeld)
+	{
+		// Detach from socket and re-enable physics
+		PropAlreadyHeld->GetRootComponent()->DetachFromComponent(FDetachmentTransformRules::KeepWorldTransform);
+		PropAlreadyHeld->GetMesh()->SetSimulatePhysics(true);
+		PropAlreadyHeld->GetMesh()->SetCollisionResponseToChannel(ECC_Pawn, OriginalPawnECR);
+	}
+	else
+	{
+		return; // We are not holding anything; do nothing and return
+	}
+
+	// Reset the necessary member fields
+	bIsAlreadyHoldingSomething = false;
+	PropAlreadyHeld = nullptr;
+}
+
+void UGrabbingAbility::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (GrabbedComponent)
+	{
+		// If this prop was grabbed using the physics handle functionality...
+		if (!GrabbedComponent->GetOwner()->ActorHasTag(AttachableTag))
+		{
+			// ... update its location every tick to be in front of the character
+			SetTargetLocation(OwnerCharacter->ActorToWorld().GetLocation() + OwnerCharacter->GetActorForwardVector() * GrabDistance);
+		}
+	}
+
+}
---
+++ Source/ChronoRPG/GrabbingAbility.h
@@ -0,0 +1,65 @@
+// Copyright 2020. All rights reserved. A game prototype by CodingRook.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "GrabbingAbility.generated.h"
+
+/**
+ * Custom PhysicsHandleComponent to implement grabbing props functionality 
+ */
+UCLASS()
+class CHRONORPG_API UGrabbingAbility : public UPhysicsHandleComponent
+{
+	GENERATED_BODY()
+
+public:
+	//~ Getter methods
+
+	UFUNCTION(BlueprintPure, Category = "Grab Configuration")
+	float GetGrabDistance() const { return GrabDistance; }
+
+	UFUNCTION(BlueprintCallable, Category = "Grab Configuration")
+	void GrabProp(AActor* In_AActor);
+
+	UFUNCTION(BlueprintCallable, Category = "Grab Configuration")
+	void DropProp();
+
+	UFUNCTION(BlueprintPure, Category = "Grab Configuration")
+	bool IsAlreadyHoldingSomething() const { return bIsAlreadyHoldingSomething; }
+
+	UFUNCTION(BlueprintPure, Category = "Grab Configuration")
+	FName GetAttachableTag() const { return AttachableTag; }
+
+protected:
+	//~ Set up some space and distance parameters for the grabbing ability (Remember it is IN CENTIMETERS!)
+
+	// Minimum distance before the player can grab an object
+	UPROPERTY(EditAnywhere, Category = "Grab Configuration")
+	float GrabDistance = 100.f;
+
+	// Radius of grabbing range when facing forward
+	UPROPERTY(EditAnywhere, Category = "Grab Configuration")
+	float GrabRadius = 50.f;
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, enum ELevelTick Ticktype, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	// Private member to store the pointer to the player character
+	class AChronoRPGCharacter* OwnerCharacter;
+
+	// Tag to indicate this prop can actually be held in hand (i.e. attached to a socket in hand, like a tool or weapon etc.)
+	// Otherwise will use physicshandle functionality for other stuff for the time being (until correct animations come in!)
+	const FName AttachableTag = "Attachable";
+
+	// Store the pointer to the prop we are already holding at the moment
+	class AInteractablePropBase* PropAlreadyHeld;
+
+	// Are we holding something already?
+	bool bIsAlreadyHoldingSomething = false;
+
+	// Use this variable to store the grabbed mesh's original collision settings for the pawn channel
+	ECollisionResponse OriginalPawnECR;
+};
