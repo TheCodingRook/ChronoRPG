a430318f44c5599cb1484245b968cb047445468d
CodingRook <CodingRook@gmail.com>
2020-05-07 15:05:37 +0100

    Have an improved version of the BP portal, with a better alignment of meshes and components in the view port (e.g. adjustments relative to default scene root so that entire asset sits squarely on a flat floor with the keyboard shortcut), as well as a solution to the viewing perspective through the portal. However, I am adjusting the final yaw result for the scene capture's rotation with a hardcoded 180 degrees. Need to figure out a correct formula!!

--- Source/ChronoRPG/TimePortal.cpp
+++ Source/ChronoRPG/TimePortal.cpp
@@ -22,13 +22,13 @@ ATimePortal::ATimePortal()
 	// Set up the bounds volume for the time portal
 	PortalTrigger = CreateDefaultSubobject<UBoxComponent>("Portal Trigger");
 	PortalTrigger->SetupAttachment(DefaultRoot);
-	PortalTrigger->SetRelativeRotation(FRotator(0.f, 0.f, 90.f));
+	//PortalTrigger->SetRelativeRotation(FRotator(0.f, 0.f, 90.f));
 	
 
 	// Set up the allowthrough toggle volume for the time portal
 	AllowThroughToggle = CreateDefaultSubobject<UBoxComponent>("Through Access");
 	AllowThroughToggle->SetupAttachment(PortalTrigger);
-	AllowThroughToggle->SetRelativeLocation(FVector(0.f, 0.f, -10.f));
+	//AllowThroughToggle->SetRelativeLocation(FVector(0.f, 0.f, -10.f));
 
 	// Set up the mesh to project the portal's viewpoint on
 	PortalScreen = CreateDefaultSubobject<UStaticMeshComponent>("Portal Screeen");
@@ -38,26 +38,26 @@ ATimePortal::ATimePortal()
 	// Set up the mesh for the portal's frame
 	PortalFrame = CreateDefaultSubobject<UStaticMeshComponent>("Portal Frame");
 	PortalFrame->SetupAttachment(PortalTrigger);
-	PortalFrame->SetRelativeLocation(FVector(0.f, 100.f, 0.f));
-	PortalFrame->SetRelativeRotation(FRotator(90.f, 0.f, -90.f));
+	//PortalFrame->SetRelativeLocation(FVector(0.f, 100.f, 0.f));
+	//PortalFrame->SetRelativeRotation(FRotator(90.f, 0.f, -90.f));
 
 	// Set up the spawn point for this time portal and its "helper" children components
 	TeleportLocation = CreateDefaultSubobject<USceneComponent>("Teleport Location");
 	TeleportLocation->SetupAttachment(DefaultRoot);
-	TeleportLocation->SetRelativeLocation(FVector(0.f, -1000.f, -100.f));
-	TeleportLocation->SetRelativeRotation(FRotator(0.f, 50.f, 0.f));
+	//TeleportLocation->SetRelativeLocation(FVector(0.f, -1000.f, -100.f));
+	//TeleportLocation->SetRelativeRotation(FRotator(0.f, 50.f, 0.f));
 
 
 	// Set up the portal's arrow component to help with placement in the Editor
 	TeleportLocationArrow = CreateDefaultSubobject<UArrowComponent>("Spawn Direction");
 	TeleportLocationArrow->SetupAttachment(TeleportLocation);
-	TeleportLocationArrow->SetRelativeLocation(FVector(70.f, 0.f, 40.f));
-	TeleportLocationArrow->SetRelativeRotation(FRotator(-30.f, 180.f, 0.f));
+	//TeleportLocationArrow->SetRelativeLocation(FVector(70.f, 0.f, 40.f));
+	//TeleportLocationArrow->SetRelativeRotation(FRotator(-30.f, 180.f, 0.f));
 
 	// Set up a text label for the spawn point so it is visible in the Editor (but not in game obviously!)
 	TeleportLocationText = CreateDefaultSubobject<UTextRenderComponent>("Spawn Point Label");
 	TeleportLocationText->SetupAttachment(TeleportLocation);
-	TeleportLocationText->SetRelativeRotation(FRotator(90.f, 0.f, 0.f));
+	//TeleportLocationText->SetRelativeRotation(FRotator(90.f, 0.f, 0.f));
 	TeleportLocationText->SetText(FText::FromString("Teleport spawn point"));
 	TeleportLocationText->SetHiddenInGame(true);
 	// Some formatting here...
@@ -68,8 +68,8 @@ ATimePortal::ATimePortal()
 	// Set up the the portal's scene capture
 	PortalSceneCapture = CreateDefaultSubobject<USceneCaptureComponent2D>("Portal SceneCapture");
 	PortalSceneCapture->SetupAttachment(TeleportLocation);
-	PortalSceneCapture->SetRelativeLocation(FVector(0.f, 0.f, 75.f));
-	PortalSceneCapture->SetRelativeRotation(FRotator(0.f, 0.f, 0.f));
+	//PortalSceneCapture->SetRelativeLocation(FVector(0.f, 0.f, 75.f));
+	//PortalSceneCapture->SetRelativeRotation(FRotator(0.f, 0.f, 0.f));
 
 }
 
